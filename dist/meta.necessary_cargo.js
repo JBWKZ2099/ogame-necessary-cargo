33bd9ede242b1b8a2eb8e7ad2be954c067360844
Ivan Ramírez <ivan_amigue@hotmail.com>
2022-06-09 12:43:59 -0500

    meta updates

--- dist/meta.necessary_cargo.js
+++ dist/meta.necessary_cargo.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Necessary cargo ships
-// @namespace    https://github.com/JBWKZ2099/ogame-necessary-cargo
+// @namespace    necessary_cargo
 // @version      1.2
 // @description  Displays necessary cargo ships to move / transport the resources
 // @author       JBWKZ2099
+// @homepageURL  https://github.com/JBWKZ2099/ogame-necessary-cargo
 // @updateURL    https://raw.githubusercontent.com/JBWKZ2099/ogame-necessary-cargo/master/dist/meta.remaining_fields.js
 // @downloadURL  https://raw.githubusercontent.com/JBWKZ2099/ogame-necessary-cargo/master/dist/user.remaining_fields.js
 // @supportURL   https://github.com/JBWKZ2099/ogame-necessary-cargo/issues
